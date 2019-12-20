20330c92e6d10f7b8f6dc80adeee2e2a7e606799
lauraherrero <lauraherrero36@gmail.com>
2019-12-20 17:06:51 +0100

    Generate docs

---
+++ docs/precache-manifest.eb3a176a982d478932db9e37783791b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0fedb4c40304efdb6608afcdd7cf1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f98cc90850956bc00b24",
+    "url": "/static/css/main.2209d685.chunk.css"
+  },
+  {
+    "revision": "31a551f93b1fb45368d2",
+    "url": "/static/js/2.5881da88.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5881da88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f98cc90850956bc00b24",
+    "url": "/static/js/main.fd243c76.chunk.js"
+  },
+  {
+    "revision": "2122e0a699fb0c075f95",
+    "url": "/static/js/runtime-main.f54d51d4.js"
+  }
+]);
