1541a3c6c2ca006d151a3edac9ec66aa3b39a4ac
lauraherrero <lauraherrero36@gmail.com>
2020-01-03 16:09:44 +0100

    Merge

---
+++ docs/build/precache-manifest.d5254923afb3ad16d9b843477a53583b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5f123b0c847088b050e20412f654c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352da99f2160fd59a12e",
+    "url": "/static/css/main.2209d685.chunk.css"
+  },
+  {
+    "revision": "31a551f93b1fb45368d2",
+    "url": "/static/js/2.5881da88.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5881da88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "352da99f2160fd59a12e",
+    "url": "/static/js/main.28e96158.chunk.js"
+  },
+  {
+    "revision": "2122e0a699fb0c075f95",
+    "url": "/static/js/runtime-main.f54d51d4.js"
+  }
+]);
